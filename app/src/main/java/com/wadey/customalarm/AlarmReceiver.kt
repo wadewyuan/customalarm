45b7dd6477cf4a9f078ae44c2cd7538dd1f38967
Wade Yuan <wadeyuanwy@hotmail.com>
2018-04-04 22:57:57 +0800

    onClick events, pending indent and alarm manager

---
+++ app/src/main/java/com/wadey/customalarm/AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package com.wadey.customalarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("[WY DEBUG]", "We're here and received !")
+    }
+}
--- app/src/main/java/com/wadey/customalarm/MainActivity.kt
+++ app/src/main/java/com/wadey/customalarm/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.wadey.customalarm
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.TimePicker
@@ -12,28 +14,49 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var timePicker : TimePicker
-    lateinit var alarmTextView : TextView
-    lateinit var alarmOnButton : Button
-    lateinit var alarmOffButton : Button
+    private lateinit var timePicker: TimePicker
+    private lateinit var alarmTextView: TextView
+    private lateinit var alarmOnButton: Button
+    private lateinit var alarmOffButton: Button
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // get alarm manager
-        var alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         // initialize the components
-        timePicker  = findViewById(R.id.timePicker)
-        alarmTextView  = findViewById(R.id.alarmText)
-        alarmOnButton  = findViewById(R.id.alarmOn)
-        alarmOffButton  = findViewById(R.id.alarmOff)
-
-        alarmOnButton.setOnClickListener({ setAlarmText("Alarm On")})
-        alarmOffButton.setOnClickListener { setAlarmText("Alarm Off") }
+        timePicker = findViewById(R.id.timePicker)
+        alarmTextView = findViewById(R.id.alarmText)
+        alarmOnButton = findViewById(R.id.alarmOn)
+        alarmOffButton = findViewById(R.id.alarmOff)
 
         // get calendar instance
-        var calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance()
+        val intent = Intent(this, AlarmReceiver::class.java)
+
+        alarmOnButton.setOnClickListener {
+            calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
+            calendar.set(Calendar.MINUTE, timePicker.minute)
+            // String template & Trinocular operation in Kotlin
+            val hourString = "${timePicker.hour}"
+            val minuteString = if (timePicker.minute >= 10) "${timePicker.minute}" else "0${timePicker.minute}"
+
+            Log.d("[WY DEBUG]", "$hourString:$minuteString")
+            setAlarmText("Alarm On $hourString:$minuteString")
+
+            // create a pending intent that delays the intent until specified calendar time
+            pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            // set alarm manager to wake up after calendar time
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        }
+        alarmOffButton.setOnClickListener {
+            setAlarmText("Alarm Off")
+            alarmManager.cancel(pendingIntent)
+            Log.d("[WY DEBUG]", "Alarm canceled.")
+        }
     }
 
     private fun setAlarmText(s: String) {
