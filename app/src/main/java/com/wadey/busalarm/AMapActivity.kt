6e2e84199c2f59f90d2dba6985ff9330554b9f12
Wade Yuan <wadeyuanwy@hotmail.com>
2018-07-19 23:40:40 +0800

    3d map enabled

--- app/src/main/java/com/wadey/busalarm/AMapActivity.kt
+++ app/src/main/java/com/wadey/busalarm/AMapActivity.kt
@@ -1,19 +1,65 @@
 package com.wadey.busalarm
 
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.amap.api.maps2d.MapView
+import android.util.Log
+import com.amap.api.maps.AMap
+import com.amap.api.maps.MapView
+import com.amap.api.maps.model.MyLocationStyle
 
-class AMapActivity : AppCompatActivity() {
 
-    private lateinit var mapView: MapView
+class AMapActivity : AppCompatActivity(), AMap.OnMyLocationChangeListener {
+
+    private lateinit var mMapView: MapView
+    private lateinit var myLocationStyle: MyLocationStyle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_amap)
 
-        mapView = findViewById(R.id.map)
-        mapView.onCreate(savedInstanceState)
-        var aMap = mapView.map
+        // Initialize map view
+        mMapView = findViewById(R.id.map)
+        mMapView.onCreate(savedInstanceState)
+        var aMap = mMapView.map
+        // Initialize my location style
+        myLocationStyle = MyLocationStyle()
+        myLocationStyle.interval(2000)
+        myLocationStyle.showMyLocation(true)
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)
+
+        // Initialize the location service
+        aMap.myLocationStyle = myLocationStyle
+        aMap.uiSettings.isMyLocationButtonEnabled = true
+        aMap.isMyLocationEnabled = true
+
+    }
+
+    override fun onMyLocationChange(loc: Location?) {
+        Log.d("[WY DEBUG]", "My location: Latitude-${loc?.latitude} Longtitude-${loc?.longitude}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // destroy the map when activity is destroyed
+        mMapView.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // resume the map when activity is resumed
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // pause the map when activity is paused
+        mMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // save map's instance state
+        mMapView.onSaveInstanceState(outState)
     }
 }
